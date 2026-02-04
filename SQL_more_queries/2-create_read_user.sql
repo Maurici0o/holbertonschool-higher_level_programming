951d61ca6d8cb21a8bfafc92ef48fa00bf76aca7
Maurici0o <mauricio_lemus@anahuac.mx>
2026-02-03 18:15:09 -0600

    .

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- Write a script that creates the database and the user.
 CREATE DATA BASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
