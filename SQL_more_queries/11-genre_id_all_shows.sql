1cb172eaa345600eca4581ad28f0eb250c7c6c1b
Maurici0o <mauricio_lemus@anahuac.mx>
2026-02-05 11:51:29 -0600

    .

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,5 @@
  SELECT tv_shows.title, tv_shows_genres.genre.id
  FROM tv_shows
  LEFT JOIN tv_shows_genres
-    ON tv_shows.id = tv_shows_genres.show_id
- ORDER BY tv_shows.title, tv_shows_genres.genre.id ASC;
+ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title, tv_shows_genres.genre.id;
